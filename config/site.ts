8d364bb7e49188d345fb820efa368fabf7d93a88
Richard Fontein <32132657+fontcodes@users.noreply.github.com>
2023-09-20 23:59:01 +0100

    feat: Add base site metadata

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+    name: "Richard Fontein's blog",
+    url: "https://font.codes",
+    ogImage: "https://font.codes/og.jpg",
+    creator: "Richard Fontein",
+    handle: "@fontcodes",
+    description:
+        "Personal blog for Richard Fontein. I write about web development, developer experience, and the future.",
+    links: {
+        twitter: "https://twitter.com/fontcodes",
+        github: "https://github.com/fontcodes/font.codes",
+    },
+}
+
+export type SiteConfig = typeof siteConfig
