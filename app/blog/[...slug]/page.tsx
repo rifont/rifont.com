9f08beb7329101b20a35d9fce6ad9a7226561425
Richard Fontein <32132657+rifont@users.noreply.github.com>
2023-09-22 11:39:10 +0100

    style: Reduce blog header size

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -51,7 +51,7 @@ export default async function BlogPage({ params }: PostProps) {
 
   return (
     <article className="py-6 prose dark:prose-invert">
-      <h1 className="mb-2">{blog.title}</h1>
+      <h2 className="mb-2">{blog.title}</h2>
       {blog.description && (
         <p className="text-xl mt-0 text-slate-700 dark:text-slate-200">
           {blog.description}
